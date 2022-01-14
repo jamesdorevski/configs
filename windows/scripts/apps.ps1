300c2e1f809f76b21e38894f5360c3825648d765
James Dorevski <james.dorevski@carsales.com.au>
2022-01-14 17:12:40 +1100

    feat: added gitconfig and windows apps.ps1

---
+++ windows/scripts/apps.ps1
@@ -0,0 +1,23 @@
+# ncli
+winget install --id Microsoft.dotnet --accept-package-agreements
+winget install --id Microsoft.PowerShell --accept-package-agreements
+winget install --id Git.Git --accept-package-agreements
+winget install --id Amazon.AWSCLI --accept-package-agreements
+
+# gui
+winget install --id Spotify.Spotify --accept-package-agreements
+winget install --id Google.Chrome --accept-package-agreements
+winget install --id Docker.DockerDesktop --accept-package-agreements
+winget install --id 7zip.7zip --accept-package-agreements
+winget install --id Notepad++.Notepad++ --accept-package-agreements
+winget install --id Postman.Postman --accept-package-agreements
+winget install --id Microsoft.WindowsTerminal --accept-package-agreements
+winget install --id Microsoft.VisualStudioCode --accept-package-agreements
+winget install --id Microsoft.PowerToys --accept-package-agreements
+winget install --id voidtools.Everything --accept-package-agreements
+winget install --id Transmission.Transmission --accept-package-agreements
+winget install --id WinDirStat --accept-package-agreements
+winget install --id VideoLAN.VLC --accept-package-agreements
+winget install --id JetBrains.Rider --accept-package-agreements
+winget install --id JetBrains.Datagrip --accept-package-agreements
+winget install --id SlackTechnologies.Slack --accept-package-agreements
