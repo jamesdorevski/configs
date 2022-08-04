bda147b27917e41af0f2fb7e3a2f772284a476b0
James Dorevski <james@jamesdorevski.com>
2022-08-05 07:22:20 +1000

    chore: added spotify.sj

---
+++ ubuntu/apps/apps.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+# CLI
+sudo apt -y install git
+sudo apt -y install tmux
+sudo apt -y install neovim
+
+# GUI
+sudo apt -y install vlc
+sudo apt -y install transmission
--- ubuntu/scripts/apps.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-# CLI
-sudo apt -y install git
-
-# GUI
-sudo apt -y install vlc
