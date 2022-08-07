311bb91700162dfbfc2730908321043d72055168
James Dorevski <james@jamesdorevski.com>
2022-08-07 17:36:22 +1000

    updated apps

--- ubuntu/apps/apps.sh
+++ ubuntu/apps/apps.sh
@@ -1,12 +1,16 @@
 #! /usr/bin/bash
 
 # CLI
-sudo apt -y install git
-sudo apt -y install tmux
-sudo apt -y install neovim
-sudo apt -y install fish
-sudo apt -y install fzf
+sudo apt -y install git \
+	tmux \
+	neovim \
+	fish \
+	fzf \
+	redshift \
+	htop \
+	light
 
 # GUI
-sudo apt -y install vlc
-sudo apt -y install transmission
+audo apt -y install vlc \
+	transmission \
+	bspwm 
