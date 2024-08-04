59e90cd626a884127bbd14e0b034c027e806f4cc
James Dorevski <james.dorevski@lumachain.io>
2024-08-05 08:52:55 +1000

    feat: added windows apps

--- windows/apps/apps.ps1
+++ windows/apps/apps.ps1
@@ -1,30 +1,28 @@
 # CLI
-winget install --id Microsoft.dotnet --accept-package-agreements
+winget install --id Microsoft.DotNet.SDK.8 --accept-package-agreements
 winget install --id Microsoft.PowerShell --accept-package-agreements
 winget install --id Git.Git --accept-package-agreements
-winget install --id Amazon.AWSCLI --accept-package-agreements
 winget install --id Neovim.Neovim --accept-package-agreements
+winget install --id Python.Python.3.12 --accept-package-agreements
 
 # GUI
 winget install --id Spotify.Spotify --accept-package-agreements
 winget install --id Google.Chrome --accept-package-agreements
-winget install --id Mozilla.Firefox --accept-package-agreements
 winget install --id Docker.DockerDesktop --accept-package-agreements
 winget install --id 7zip.7zip --accept-package-agreements
 winget install --id Notepad++.Notepad++ --accept-package-agreements
-winget install --id Postman.Postman --accept-package-agreements
-winget install --id Microsoft.WindowsTerminal --accept-package-agreements
 winget install --id Microsoft.VisualStudioCode --accept-package-agreements
 winget install --id Microsoft.PowerToys --accept-package-agreements
 winget install --id voidtools.Everything --accept-package-agreements
 winget install --id WinDirStat --accept-package-agreements
 winget install --id VideoLAN.VLC --accept-package-agreements
-winget install --id JetBrains.Rider --accept-package-agreements
-winget install --id JetBrains.Datagrip --accept-package-agreements
-winget install --id SlackTechnologies.Slack --accept-package-agreements
 winget install --id LINQPad.LINQPad.7 --accept-package-agreements
-winget install --id Zoom.Zoom --accept-package-agreements
-winget install --id Discord.Discord --accept-package-agreements
-winget install --id Insomnia.Insomnia --accept-package-agreements
 winget install --id TheDocumentFoundation.LibreOffice --accept-package-agreements
 winget install --id qBittorrent.qBittorrent --accept-package-agreements
+winget install --id Mozilla.Thunderbird --accept-package-agreements
+winget install --id DevToys-app.DevToys --accept-package-agreements
+winget install --id Obsidian.Obsidian --accept-package-agreements
+winget install --id Microsoft.Sysinternals.ZoomIt --accept-package-agreements
+winget install --id Microsoft.Teams --accept-package-agreements
+winget install --id Fork.Fork --accept-package-agreements
+
