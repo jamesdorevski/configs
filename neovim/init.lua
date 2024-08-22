ba15b7177e5b3abde530b3c83ffa26190c474ce3
James Dorevski <james.dorevski@lumachain.io>
2024-08-23 09:08:28 +1000

    sync

---
+++ neovim/init.lua
@@ -0,0 +1,9 @@
+require("config.lazy")
+require("config.cmp")
+require("config.lsp-zero")
+require("config.telescope")
+require("config.treesitter")
+require("config.fugitive")
+
+require("config.set")
+require("config.remap")
