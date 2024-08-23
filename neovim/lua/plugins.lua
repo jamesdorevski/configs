679333a77521761d08786a4353818b2c1a07c2bc
James Dorevski <james.dorevski@lumachain.io>
2024-08-23 12:06:28 +1000

    sync: added nvim

---
+++ neovim/lua/plugins.lua
@@ -0,0 +1,22 @@
+return {
+	{ 
+		"xiantang/darcula-dark.nvim", 
+		init = function()
+			vim.cmd("colorscheme darcula-dark")
+		end
+	},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'github/copilot.vim'},
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {"tpope/vim-fugitive"}
+}
+
