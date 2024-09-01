c983268699ded39eb88ba488ed4d58b02f092a11
James Dorevski <james.dorevski@lumachain.io>
2024-09-02 07:23:36 +1000

    chore: added neo tree to nvim

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -1,22 +1,22 @@
 return {
-	{ 
-		"xiantang/darcula-dark.nvim", 
-		init = function()
-			vim.cmd("colorscheme darcula-dark")
-		end
-	},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'github/copilot.vim'},
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    { 
+        "xiantang/darcula-dark.nvim", 
+        init = function()
+            vim.cmd("colorscheme darcula-dark")
+        end
+    },
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'github/copilot.vim'},
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
     {"tpope/vim-fugitive"},
     {"mfussenegger/nvim-dap"},
     {
@@ -24,6 +24,14 @@ return {
         branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" }
     },
-    {'hiphish/rainbow-delimiters.nvim'}
+    {'hiphish/rainbow-delimiters.nvim'},
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        }
+    }
 }
 
