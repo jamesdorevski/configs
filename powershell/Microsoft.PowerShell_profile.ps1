2e50bacea2a2c06acd1aa79e3798a8bca0a26566
James Dorevski <james.dorevski@lumachain.io>
2024-09-03 08:09:33 +1000

    sync

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,18 @@ Invoke-Expression (&jump shell pwsh | Out-String)
 
 Set-PSReadLineOption -EditMode Vi
 
+function OnViModeChange {
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    } else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
+
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
+
 if (Test-Path "C:\Users\micro\.jabba\jabba.ps1") { . "C:\Users\micro\.jabba\jabba.ps1" }
 
 
