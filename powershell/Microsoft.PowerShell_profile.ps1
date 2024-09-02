60b1ea86dc9215f47d833f0afb9d7e475fc09e22
James Dorevski <james.dorevski@lumachain.io>
2024-09-03 08:07:13 +1000

    feat (powershell): added new profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,8 @@
-# Aliases
-Set-Alias -Name vi -Value nvim
-Set-Alias -Name files -Value explorer
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+Invoke-Expression (&jump shell pwsh | Out-String)
 
-# Enable Starship
-Invoke-Expression (&starship init powershell)
-
-# Modules
-Import-Module -Name Terminal-Icons
-
-# PsReadLine settings
-Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -EditMode Vi
+
+if (Test-Path "C:\Users\micro\.jabba\jabba.ps1") { . "C:\Users\micro\.jabba\jabba.ps1" }
+
+
