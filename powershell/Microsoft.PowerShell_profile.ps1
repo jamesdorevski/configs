d32fe9b9efdfcac414781811abce7f0f7464467c
James Dorevski <james.dorevski@lumachain.io>
2024-09-03 15:15:19 +1000

    chore: set ps history to list view

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,7 @@
 Import-Module -Name Microsoft.WinGet.CommandNotFound
 Invoke-Expression (&jump shell pwsh | Out-String)
 
-Set-PSReadLineOption -EditMode Vi
-
-function OnViModeChange {
-    if ($args[0] -eq 'Command') {
-        # Set the cursor to a blinking block.
-        Write-Host -NoNewLine "`e[1 q"
-    } else {
-        # Set the cursor to a blinking line.
-        Write-Host -NoNewLine "`e[5 q"
-    }
-}
-
-Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
+Set-PSReadLineOption -PredictionViewStyle ListView
 
 if (Test-Path "C:\Users\micro\.jabba\jabba.ps1") { . "C:\Users\micro\.jabba\jabba.ps1" }
 
