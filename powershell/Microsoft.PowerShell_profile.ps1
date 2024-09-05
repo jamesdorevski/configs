4b88691eeb3d1d2286f01a996705807d0245d597
James Dorevski <james.dorevski@lumachain.io>
2024-09-05 14:52:28 +1000

    added cursor config

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -5,4 +5,4 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 
 if (Test-Path "C:\Users\micro\.jabba\jabba.ps1") { . "C:\Users\micro\.jabba\jabba.ps1" }
 
-
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/emodipt-extend.omp.json" | Invoke-Expression
